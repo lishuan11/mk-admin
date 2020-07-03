6c714740cf0909d97d22655d772ce196ccb3fca0
LISHUAN <1196741342@qq.com>
2020-07-03 10:38:30 +0800

    fd

---
+++ mk-consumer-web-80/src/main/java/com/lishuan/mk/MkConsumerWeb80Application.java
@@ -0,0 +1,13 @@
+package com.lishuan.mk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkConsumerWeb80Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkConsumerWeb80Application.class, args);
+    }
+
+}
